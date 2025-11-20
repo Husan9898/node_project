dabee4aed31e604f0c58e9a917e637fbe0389173
Husan9898 <husanpreetsingh.lnu@rdpolytech.ca>
2025-11-20 11:05:45 -0700

    update

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const app = express();
-const router = require('./router/pages.js');
+const router = require('./routes/pages.js');
+const userRoute = require('./routes/users.js');
+const ejs = require('ejs');
 const port = 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+app.use(router);
+app.use(userRoute);
 
- app.use(router);
-
-
-
-app.listen(port, ()=>{
+app.listen(port, () =>{
     console.log(`Server is running on http://localhost:${port}`)
-
 });
