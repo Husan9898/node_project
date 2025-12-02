bb54344cdbcf6fd9dbf3b3a2fcbb5886e2573e56
Husan9898 <husanpreetsingh.lnu@rdpolytech.ca>
2025-12-02 10:29:21 -0700

    update

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const pagesRouter = require('./routes/pages.js');
 const userRoute = require('./routes/users.js');
 const ejs = require('ejs');
 const rateLimit = require('express-rate-limit');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const url = process.env.MONGODB_URI;
 const port = 3000;
+
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
@@ -21,6 +25,14 @@ app.use(pagesRouter);
 app.use(userRoute);
 app.use(express.static('public'));
 
+mongoose.connect(url)
+.then(() => {
+    console.log("connected to mongoose");
+
 app.listen(port, () =>{
     console.log(`Server is running on http://localhost:${port}`)
+});
+
+}).catch((error)=>{
+    console.error("Error connecting to MongoDB:", error);
 });
